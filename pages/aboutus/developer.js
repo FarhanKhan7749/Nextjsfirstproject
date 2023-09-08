6401953e5ac26b1adced0f95d6cdcac9dcd590d6
farhan khan <fk828633@gmail.com>
2023-09-08 20:30:02 +0530

    nestd pages in next js

---
+++ pages/aboutus/developer.js
@@ -0,0 +1,8 @@
+
+function DevName(){
+    return <h1>
+      My name is farhan khan. i am a zoho developer.
+    </h1>
+  }
+  
+  export default DevName;
---
+++ pages/news/something-importent.js
@@ -0,0 +1,8 @@
+
+function DetailPage(){
+    return <h1>
+      this is detail page
+    </h1>
+  }
+  
+  export default DetailPage;
